e0b9aa91706f39c8cb0d76088845e7dfa3168d19
Hamza <hamzaabhutta@gmail.com>
2023-08-15 12:49:37 +0500

    resolveing api

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import TopButton from "./components/TopButton";
 import InputField from "./components/InputField";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails";
-
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/WeatherService";
 
 function App() {
+  // Async function to fetch weather data and log it
+  const fetchWeather = async () => {
+    try {
+      const data = await getFormattedWeatherData({ q: "london" });
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  // Call the fetchWeather function to initiate data fetching
+  fetchWeather();
+
+  // Return JSX for rendering the UI components
   return (
     <>
-      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
+      <div className="mx-auto max-w-screen-md my-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
         <TopButton />
-        <InputField/>
-        <TimeAndLocation/>
-        <TempAndDetails/>
+        <InputField />
+        <TimeAndLocation />
+        <TempAndDetails />
+        <Forecast title="hourly forecast" />
+        <Forecast title="daily forecast" />
       </div>
     </>
   );
---
+++ src/services/WeatherService.js
@@ -0,0 +1,166 @@
+import { DateTime } from "luxon";
+
+const API_KEY = "d6589f659aca8c6ac8c02f857f5d6201";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// const getWeatherData = (infoType, searchParams) => {
+//   const url = new URL(BASE_URL + "/" + infoType);
+//   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+//   return fetch(url).then((res) => res.json());
+// };
+
+const getWeatherData = async (infoType, searchParams) => {
+  const url = new URL(`${BASE_URL}/${infoType}`);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    throw error;
+  }
+};
+
+
+
+///destructuring the array data coming from api
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+  const { main: details, icon } = weather[0];
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+
+
+// const formatForecastWeather = (data) => {
+//   let { timezone, daily, hourly } = data;
+//   daily = daily.slice(1, 6).map((d) => {
+//     return {
+//       title: formatToLocalTime(d.dt, timezone, "ccc"),
+//       temp: d.temp.day,
+//       icon: d.weather[0].icon,
+//     };
+//   });
+
+//   hourly = hourly.slice(1, 6).map((d) => {
+//     return {
+//       title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+//       temp: d.temp.day,
+//       icon: d.weather[0].icon,
+//     };
+//   });
+
+//   return { timezone, daily, hourly };
+// };
+
+const formatForecastWeather = (data) => {
+  let { timezone, daily, hourly } = data;
+
+  // Make sure daily and hourly arrays are defined before using slice and map
+  if (daily && hourly) {
+    daily = daily.slice(1, 6).map((d) => {
+      return {
+        title: formatToLocalTime(d.dt, timezone, "ccc"),
+        temp: d.temp.day,
+        icon: d.weather[0].icon,
+      };
+    });
+
+    hourly = hourly.slice(1, 6).map((d) => {
+      return {
+        title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+        temp: d.temp.day,
+        icon: d.weather[0].icon,
+      };
+    });
+  } else {
+    // Handle the case where daily or hourly arrays are not defined as expected
+    daily = [];
+    hourly = [];
+  }
+
+  return { timezone, daily, hourly };
+};
+
+
+
+
+// const getFormattedWeatherData = async (searchParams) => {
+//   const formattedCurrentWeather = await getWeatherData(
+//     "weather",
+//     searchParams
+//   ).then(formatCurrentWeather);
+
+//   const { lat, lon } = formatCurrentWeather;
+
+//   const formattedForcastWeather = await getWeatherData("oncall", {
+//     lat,
+//     lon,
+//     exclude: "current,minutely,alerts",
+//     units:searchParams.units,
+//   }).then(formatForecastWeather);
+//   return {...formatCurrentWeather,...formatForecastWeather};
+// };
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await formatCurrentWeather(
+    await getWeatherData("weather", searchParams)
+  );
+
+  const { lat, lon } = formattedCurrentWeather; // Not await here
+
+  const formattedForecastWeather = await formatForecastWeather(
+    await getWeatherData("oncall", {
+      lat,
+      lon,
+      exclude: "current,minutely,alerts",
+      units: searchParams.units,
+    })
+  );
+
+  return { ...formattedCurrentWeather, ...formattedForecastWeather };
+};
+
+
+
+
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc,dd LLL yyyy' | Local time:'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconUrlFromCode=(code)=>`http://openweathermap.org/img/wn/${code}@2x.png`
+
+export {formatToLocalTime,iconUrlFromCode,getFormattedWeatherData}
+
+export default getFormattedWeatherData;
